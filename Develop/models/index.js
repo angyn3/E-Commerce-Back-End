f58c535a68eed4bbf1b098ba8863a400041ad8d5
Andy Nguyen <nguyen3andy@gmail.com>
2023-08-04 19:46:16 -0400

    completed index file

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -7,13 +7,26 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category,{
   foreignKey:"category_id",
-  onDelete: 'CASCADE',
+  onDelete:'CASCADE',
 })
 
 // Categories have many Products
-
+Category.hasMany(Product,{
+  foreignKey:"category_id",
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  through:ProductTag,
+  foreignKey:"product_id",
+  onDelete:'CASCADE',
+})
+
+Tag.belongsToMany(Product,{
+  through:ProductTag,
+  foreignKey:"tag_id",
+  onDelete:'CASCADE'
+})
 
 // Tags belongToMany Products (through ProductTag)
 
