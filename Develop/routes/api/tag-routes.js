c6fbf1eca068c8fa8a46222d25798395eb5c4397
Andy Nguyen <nguyen3andy@gmail.com>
2023-08-04 22:47:24 -0400

    completed api js files

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,26 +3,78 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+// find all tags  
+  try {
+    const tagData = await Tag.findAll({
+      include: [{model: Product}]
+    })
+    res.status(200).json(tagsData)
+  }
+  catch (err) {
+    console.log(err)
+    res.status(500).json ({error: 'Internal Server Error'})
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+// find a single tag by its `id`
+  try {
+    const tagsData = await Tag.findByPk(req.params.id, {
+      include: [{model: Product}]
+    })
+    res.status(200).json(tagsData)
+  }
+  catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }  
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+// create a new tag
+  try {
+    const tagsData = await Tag.create({
+      tag_name: req.body.tag_name
+    })
+    res.status(200).json(tagsData)
+  }
+  catch(err) {
+    console.log(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+// update a tag's name by its `id` value
+  tag {
+    const tagsData = await Tag.update ({
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id:req.params.id
+      }
+    })
+    res.status(200).json("Successfully Updated")
+  }
+  catch (err) {
+    console.log(err)
+  }
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+// delete on tag by its `id` value
+  try {
+    const tagsData = await Tag.destroy ({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json("Successfully Deleted")
+  }
+  catch (err) {
+    console.log(err)
+  }
 });
 
 module.exports = router;
